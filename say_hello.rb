5a312dbce0567e92f927b768feedf466ec6b3f87
sarah hussain <sarahhussain17@yahoo.com>
2020-04-20 23:24:31 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,5 @@
-
 def say_hello(name)
   puts "Hello,#{name}!"
 end
 
-say_hello("Gabriela")
-
-def say_hello(name, language="Ruby")
-  puts "Hello #{name}, #{language} programmer!"
-end
-say_hello("Gabriela","Ruby")
+say_hello ("Gabriela")
